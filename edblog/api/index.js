ef5d33cff0a4ac20b916885cd77f86bf3452126c
isteguven <isteguven@yandex.com>
2024-05-21 12:00:57 +0300

    monngodb

--- edblog/api/index.js
+++ edblog/api/index.js
@@ -1,6 +1,21 @@
  import express from 'express';
+ import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose.connect (
+    process.env.MONGO
+).then(
+    () =>{console.log('Veritabanı bağlantısı sağlandı!');
+            }).catch(err=>{
+                console.log(err);
+            })
 
  const app = express();
+ 
+ 
+ 
+
 
  app.listen(3001, () =>{
     console.log('Sunucu 3001 portunda çalışıyor! ');
