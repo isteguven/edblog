4c908836753f878a9e56c5dda9a2e0a8317c83e0
isteguven <isteguven@yandex.com>
2024-05-22 23:41:50 +0300

    signup functionality

--- edblog/client/src/pages/SignUp.jsx
+++ edblog/client/src/pages/SignUp.jsx
@@ -1,7 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import login from "../assets/login.png";
-import { Badge, Button, Label, TextInput } from "flowbite-react";
-const SignUp = () => {
+import { Alert, Badge, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+export default function  SignUp () {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+   setFormData({ ...formData, [e.target.id]: e.target.value.trim()});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Lütfen tüm alanları eksiksiz doldurunuz!');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/signin');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -18,38 +55,51 @@ const SignUp = () => {
           </p>
         </div>
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form onSubmit={handleSubmit}
+          className="flex flex-col gap-4">
             <div>
               <Label value="Kullanıcı adı" />
               <TextInput
                 type="text"
                 placeholder="kullanıcı adınız..."
-                id="username"
+                id="username"   onChange={handleChange}
               />
             </div>
             <div>
               <Label value="E-posta adresi" />
               <TextInput
-                type="text"
+                type="email"
                 placeholder="isim@firma.com"
-                id="email"
+                id="email"  onChange={handleChange}
               />
             </div>
             <div>
               <Label value="Şifre" />
-              <TextInput type="text" placeholder="şifreniz..." id="password" />
+              <TextInput type="password" placeholder="şifreniz..." id="password" onChange={handleChange}/>
             </div>
-            <Button gradientDuoTone='purpleToBlue' type='submit' outline>
-              Kayıt Ol
+            <Button gradientDuoTone='purpleToBlue' type='submit' outline disabled={loading}>
+              { 
+              loading ?  (
+                <>
+                <Spinner size='sm'/>
+                <span className="pl-3">Yükleniyor...</span>
+                </>
+              ) : 'Kayıt Ol' 
+            }
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5 ">
           <span>Hesabınız var mı? </span>
           <Link to='/signin' className="text-blue-500">Giriş</Link></div>
         </div>
+        {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
       </div>
     </div>
   );
-};
+}
 
-export default SignUp;
+
