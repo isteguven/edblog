491abcf9cb8d6694e8cd436b0228acd1475ac7ed
isteguven <isteguven@yandex.com>
2024-05-23 19:32:49 +0300

    signin

--- edblog/client/src/App.jsx
+++ edblog/client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Dashboard from './pages/Dashboard'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <Route path ='/signup' element={<SignUp/>}/>
       <Route path ='/dashboard' element={<Dashboard/>}/>
     </Routes>
+    <Footer/>
     </BrowserRouter>
 
   )
--- edblog/client/src/components/Footer.jsx
+++ edblog/client/src/components/Footer.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
-
-const Footer = () => {
+import { Footer } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png'
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
+export default function FooterCom() {
   return (
-    <div>Footer</div>
-  )
-}
+    <Footer container className='border border-t-8 border-sky-100'>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+          <div className='mt-5'>
+          <Link
+        to='/'
+        className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
+      >
+  <img className='w-28 sm:w-28' src={logo} alt="" />
+</Link>
+          </div>
+          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+            <div>
+              <Footer.Title title='Hakkında' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://www.100jsprojects.com'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Biz Kimiz?
+                </Footer.Link>
+                <Footer.Link
+                  href='/about'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  İletişim
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Bizi Takip Et' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://www.github.com'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href='#'>Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Yükümlülükler' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='#'>KVKK</Footer.Link>
+                <Footer.Link href='#'>Kullanıcı &amp; Sözleşmesi</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright
+            href='#'
+            by="CYZ Yazılım"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center ">
+            <Footer.Icon href='#' icon={BsFacebook}/>
+            <Footer.Icon href='#' icon={BsInstagram}/>
+            <Footer.Icon href='#' icon={BsTwitter}/>
+            <Footer.Icon href='https://github.com' icon={BsGithub}/>
+            <Footer.Icon href='#' icon={BsDribbble}/>
 
-export default Footer
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
