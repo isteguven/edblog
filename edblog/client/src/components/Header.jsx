5d4f56723c771977da08c9bb64e84b5fb5c64a39
isteguven <isteguven@yandex.com>
2024-05-25 02:56:37 +0300

    darkmodetheme

--- edblog/client/src/components/Header.jsx
+++ edblog/client/src/components/Header.jsx
@@ -1,54 +1,105 @@
-import { Button, Navbar, TextInput } from "flowbite-react"
-import { Link,useLocation } from "react-router-dom"
-import logo from '../assets/logo.png'
-import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
-export default function Header()  {
-    const path = useLocation().pathname;
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon ,FaSun} from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { toggleTheme } from "../redux/theme/themeSlice";
+export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  const path = useLocation().pathname;
+  const dispatch = useDispatch();
+  const {theme} = useSelector((state)=>state.theme);
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
-    <Navbar className='border-b-2'>
-<Link
-        to='/'
-        className='self-center whitespace-nowrap text-sm sm:text-3xl font-semibold dark:text-white'
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-3xl font-semibold dark:text-white"
       >
-  <img className='w-28 sm:w-28' src={logo} alt="" />
-</Link>
-<form action="">
-    <TextInput
-    type="text"
-    placeholder="Arama Yap..."
-    rightIcon={AiOutlineSearch}
-    className="hidden lg:inline"
-    ></TextInput>
-</form>
-<Button className="w-12 h-10 lg:hidden" color='gray' pill><AiOutlineSearch/></Button>
-<div className="flex gap-2 md:order-2 ">
-<Button className="w-12 h-10 hidden sm:inline" color='gray' pill>
-    <FaMoon/>
-</Button>
-<Link to='/signin'>
-    <Button  gradientDuoTone='purpleToBlue' outline>Giriş</Button>
-</Link>
-<Navbar.Toggle/> 
-</div>
-<Navbar.Collapse>
-    <Navbar.Link active={path==="/"} as={'div'}>
-        <Link to='/'>
-            Ana Sayfa
-        </Link>
-    </Navbar.Link>
-    <Navbar.Link active={path==="/about"} as={'div'}>
-        <Link to='/about'>
-        Hakkında
-        </Link>
-    </Navbar.Link>
-    <Navbar.Link active={path==="/projects"} as={'div'}>
-        <Link to='/projects'>
-            Projeler
-        </Link>
-    </Navbar.Link>
-</Navbar.Collapse>
-    </Navbar>
-  )
-}
+        <img className="w-28 sm:w-28" src={logo} alt="" />
+      </Link>
+      <form action="">
+        <TextInput
+          type="text"
+          placeholder="Arama Yap..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        ></TextInput>
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2 ">
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+            {theme==='light' ? <FaSun color="blue"  /> : <FaMoon  color="white"/>}
+          
+        </Button>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="kullanıcı"
+                img={currentUser.profilePicture}
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profil Bilgisi</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Çıkış</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/signin">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Giriş
+            </Button>
+          </Link>
+        )}
 
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Ana Sayfa</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">Hakkında</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projeler</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
