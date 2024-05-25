326ed0f0b5183095748261791607997f33c34393
isteguven <isteguven@yandex.com>
2024-05-25 19:18:19 +0300

    sidebar of dashboard

--- edblog/client/src/App.jsx
+++ edblog/client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn'
 import Dashboard from './pages/Dashboard'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -18,7 +19,9 @@ const App = () => {
       <Route path ='/projects' element={<Projects />}/>
       <Route path ='/signin' element={<SignIn/>}/>
       <Route path ='/signup' element={<SignUp/>}/>
+      <Route  element={<PrivateRoute/>}>
       <Route path ='/dashboard' element={<Dashboard/>}/>
+      </Route>
     </Routes>
     <Footer/>
     </BrowserRouter>
---
+++ edblog/client/src/components/DashSidebar.jsx
@@ -0,0 +1,40 @@
+import { Sidebar } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+import {
+    HiUser,
+    HiArrowSmRight,
+    HiDocumentText,
+    HiOutlineUserGroup,
+    HiAnnotation,
+    HiChartPie,
+  } from 'react-icons/hi';
+
+export default function DashSidebar() {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const tabFromUrl = urlParams.get('tab');
+      if (tabFromUrl) {
+        setTab(tabFromUrl);
+      }
+    }, [location.search]);
+  return (
+   <Sidebar className='w-full md:w-56'>
+    <Sidebar.Items>
+        <Sidebar.ItemGroup>
+            <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'Kullanıcı'} labelColor="dark">
+                Profil
+            </Sidebar.Item>
+            </Link>
+            <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer' >
+                Çıkış
+            </Sidebar.Item>
+        </Sidebar.ItemGroup>
+    </Sidebar.Items>
+   </Sidebar>
+  )
+}
