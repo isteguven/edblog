ef99219038e6b0b6f72ba8ac90a768ddd8629a7a
isteguven <isteguven@yandex.com>
2024-05-26 03:30:53 +0300

    profile image upload func

--- edblog/client/src/components/DashSidebar.jsx
+++ edblog/client/src/components/DashSidebar.jsx
@@ -1,40 +1,46 @@
-import { Sidebar } from 'flowbite-react';
-import { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Sidebar } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 import {
-    HiUser,
-    HiArrowSmRight,
-    HiDocumentText,
-    HiOutlineUserGroup,
-    HiAnnotation,
-    HiChartPie,
-  } from 'react-icons/hi';
+  HiUser,
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiAnnotation,
+  HiChartPie,
+} from "react-icons/hi";
 
 export default function DashSidebar() {
-    const location = useLocation();
-    const [tab, setTab] = useState('');
-    useEffect(() => {
-      const urlParams = new URLSearchParams(location.search);
-      const tabFromUrl = urlParams.get('tab');
-      if (tabFromUrl) {
-        setTab(tabFromUrl);
-      }
-    }, [location.search]);
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-   <Sidebar className='w-full md:w-56'>
-    <Sidebar.Items>
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
         <Sidebar.ItemGroup>
-            <Link to='/dashboard?tab=profile'>
-            <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'Kullanıcı'} labelColor="dark">
-                Profil
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"Kullanıcı"}
+              labelColor="dark"
+              as='div'
+            >
+              Profil
             </Sidebar.Item>
-            </Link>
-            <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer' >
-                Çıkış
-            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+            Çıkış
+          </Sidebar.Item>
         </Sidebar.ItemGroup>
-    </Sidebar.Items>
-   </Sidebar>
-  )
+      </Sidebar.Items>
+    </Sidebar>
+  );
 }
