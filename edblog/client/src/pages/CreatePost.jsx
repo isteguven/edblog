ab80a22de578af84b1705f4e2c354e7d3fd66396
isteguven <isteguven@yandex.com>
2024-05-29 00:41:32 +0300

    upload post img func

--- edblog/client/src/pages/CreatePost.jsx
+++ edblog/client/src/pages/CreatePost.jsx
@@ -1,20 +1,66 @@
-import {  Button, FileInput, Select, TextInput } from 'flowbite-react';
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-
-
-
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
 import { useState } from 'react';
-
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { useNavigate } from 'react-router-dom';
 
 
 export default function CreatePost() {
+  const [file, setFile] = useState(null);
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
 
-    const [formData, setFormData] = useState({});
+  const navigate = useNavigate();
 
-  
-    const navigate = useNavigate();
+  const handleUpdloadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadError('Lütfen bir imaj seçin!');
+        return;
+      }
+      setImageUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + '-' + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadError('İmaj yükleme başarısız!');
+          setImageUploadProgress(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUploadProgress(null);
+            setImageUploadError(null);
+            setFormData({ ...formData, image: downloadURL });
+          });
+        }
+      );
+    } catch (error) {
+      setImageUploadError('İmaj yükleme işlemi başarısız oldu!');
+      setImageUploadProgress(null);
+      console.log(error);
+    }
+  };
+
+
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
         <h1 className='text-center text-2xl my-7 font-semibold text-slate-700'>Yeni Yazı Oluştur</h1>
@@ -48,15 +94,38 @@ export default function CreatePost() {
             <div className='flex gap-4 items-center justify-between border-4 border-cyan-300 border-dotted p-3'>
           <FileInput
             type='file'
-            accept='image/*'        
+            accept='image/*'   
+            onChange={(e)=>setFile(e.target.files[0])}     
           />
           <Button
             type='button'
             gradientDuoTone='purpleToBlue'
             size='sm'
-            outline            
-          >İmaj Yükle</Button>
+            outline   
+            onClick={handleUpdloadImage}        
+             disabled={imageUploadProgress}
+          >
+           {imageUploadProgress ? (
+              <div className='w-16 h-16'>
+                <CircularProgressbar
+                  value={imageUploadProgress}
+                  text={`${imageUploadProgress || 0}%`}
+                />
+              </div>
+            ) : (
+              'İmaj Yükle'
+            )}           
+          
+          </Button>
           </div>
+          {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+        {formData.image && (
+          <img
+            src={formData.image}
+            alt='upload'
+            className='w-full h-96 object-cover'
+          />
+        )}
           <ReactQuill theme='snow' placeholder='Bir şeyler yaz...' className='h-72' mb-12 required/>
           <Button type='submit' gradientDuoTone='cyanToBlue' outline>
           Yayınla
