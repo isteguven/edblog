1e4d3d11d32d9dd6f82be9d0a8dd12c6c4b4d564
isteguven <isteguven@yandex.com>
2024-05-31 20:40:23 +0300

    create comment

--- edblog/client/src/pages/PostPage.jsx
+++ edblog/client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
+import CommentSection from '../components/CommentSection';
 
 
 
@@ -73,7 +74,7 @@ export default function PostPage() {
     <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
       </div> 
-
+<CommentSection postId={post._id}/>
       <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Son Yazılar</h1>
         <div className='flex flex-wrap gap-5 mt-5 justify-center'>
