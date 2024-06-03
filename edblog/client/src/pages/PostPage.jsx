8b9b1e47e7e4b00e6ec37174ab424b541547d9d5
isteguven <isteguven@yandex.com>
2024-06-03 19:25:50 +0300

    recent artciles

--- edblog/client/src/pages/PostPage.jsx
+++ edblog/client/src/pages/PostPage.jsx
@@ -1,17 +1,17 @@
-import { Button, Spinner } from 'flowbite-react';
-import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import CallToAction from '../components/CallToAction';
-import CommentSection from '../components/CommentSection';
-
-
+import { Button, Spinner } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 export default function PostPage() {
-    const { postSlug } = useParams();
+  const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
   const [recentPosts, setRecentPosts] = useState(null);
+
   useEffect(() => {
     const fetchPost = async () => {
       try {
@@ -36,13 +36,28 @@ export default function PostPage() {
     fetchPost();
   }, [postSlug]);
 
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
   if (loading)
     return (
       <div className="flex justify-center items-center min-h-screen ">
         <Spinner size="xl" />
       </div>
     );
-    
+
   return (
     <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
       <h1 className="text-2xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
@@ -51,7 +66,7 @@ export default function PostPage() {
       <Link
         to={`/search?category=${post && post.category}`}
         className="self-center mt-5"
-      > 
+      >
         <Button color="gray" outline pill size="xs">
           {post && post.category}
         </Button>
@@ -61,24 +76,28 @@ export default function PostPage() {
         alt={post && post.title}
         className="mt-10 p-3 max-h-[600px] w-full object-cover "
       />
-      <div className='flex justify-between p-3 border-b border-slate-300 mx-auto w-full max-w-2xl text-xs'>
+      <div className="flex justify-between p-3 border-b border-slate-300 mx-auto w-full max-w-2xl text-xs">
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        <span className='italic'>
-          {post && (post.content.length / 1000).toFixed(0)} dakikalık okuma         
+        <span className="italic">
+          {post && (post.content.length / 1000).toFixed(0)} dakikalık okuma
         </span>
       </div>
       <div
-        className='p-3 max-w-2xl mx-auto w-full post-content  text-slate-500'
+        className="p-3 max-w-2xl mx-auto w-full post-content  text-slate-500"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-    <div className='max-w-4xl mx-auto w-full'>
+      <div className="max-w-4xl mx-auto w-full">
         <CallToAction />
-      </div> 
-<CommentSection postId={post._id}/>
-      <div className='flex flex-col justify-center items-center mb-5'>
-        <h1 className='text-xl mt-5'>Son Yazılar</h1>
-        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
-         
+      </div>
+      <CommentSection postId={post._id} />
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5">Son Yazılar</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+          {
+            recentPosts && recentPosts.map((post)=>(
+              <PostCard key={post._id} post={post}/>
+            ))
+          }
         </div>
       </div>
     </main>
--- edblog/client/tailwind.config.js
+++ edblog/client/tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     extend: {},
   },
   // eslint-disable-next-line no-undef
-  plugins: [flowbite.plugin(),require('tailwind-scrollbar'),],
+  plugins: [flowbite.plugin(),require('tailwind-scrollbar'),require('@tailwindcss/line-clamp'),],
 }
 
 
