3ddcaa1e68891705c36e633063482ded25df1913
isteguven <isteguven@yandex.com>
2024-06-06 00:21:09 +0300

    about and projects page

--- edblog/client/src/pages/About.jsx
+++ edblog/client/src/pages/About.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
-const About = () => {
+export default function About() {
   return (
-    <div>About</div>
-  )
-}
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            Hakkımızda...
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rerum culpa illum. Debitis necessitatibus repudiandae ea obcaecati inventore, laboriosam, unde similique magni veniam, hic dolorem illo corporis est perspiciatis autem possimus! Praesentium possimus expedita recusandae neque. Exercitationem vel hic obcaecati, asperiores perferendis, eveniet corrupti eaque, qui quas sunt non. Iste, reprehenderit illo libero, saepe laboriosam dolorem et repudiandae amet enim impedit assumenda dicta quidem accusantium voluptatibus asperiores beatae exercitationem delectus quas adipisci? Facilis exercitationem voluptates reiciendis alias accusamus, eaque id cum nam, perferendis ratione aliquid adipisci magnam labore?
+            </p>
 
-export default About
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, odio! Harum veritatis unde odit sed modi consequatur ratione commodi, enim, temporibus asperiores eum dignissimos ea. Beatae sapiente, earum eveniet ducimus adipisci accusamus, in cumque sed nisi fugit, amet tempora animi perferendis ratione at quibusdam aliquid. Molestias quisquam autem earum repellendus quam illo, sed ex voluptate ipsam, eos id quaerat accusantium repellat repudiandae, recusandae cumque eaque. Ea quam deleniti praesentium, minus quibusdam aliquam. Nulla error aspernatur alias dolor recusandae id expedita ducimus! Aliquid veniam placeat fugiat, amet minus laboriosam assumenda molestias! Impedit, magnam? Deleniti, illo neque? Fugiat.
+            </p>
+
+            <p>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, harum officia magni autem at dolor minima, explicabo ea consequatur sit est totam placeat esse? Dolorum, animi expedita distinctio autem nulla quibusdam nostrum quo aliquam dolore perspiciatis. Nam libero harum deserunt exercitationem dicta sunt, veritatis quasi iste perspiciatis velit dolorem quo illo nostrum tenetur magnam magni ad excepturi corrupti facere reprehenderit aspernatur veniam placeat amet ea. At, earum deleniti.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- edblog/client/src/pages/Projects.jsx
+++ edblog/client/src/pages/Projects.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import CallToAction from '../components/CallToAction';
 
-const Projects = () => {
+export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projeler</h1>
+      <p className='text-md text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, facilis voluptates id labore harum quisquam voluptatibus officia quia eum voluptatum sequi perferendis consequuntur sapiente quos quo sed deleniti similique. Blanditiis harum excepturi exercitationem consequuntur aut, mollitia reiciendis nulla, eaque eveniet ratione et quia expedita natus rem?!</p>
+      <CallToAction />
+    </div>
   )
 }
-
-export default Projects
